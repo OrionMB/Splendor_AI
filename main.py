ca02845aae6dc340d4ff99fff41c9d5d02a0fecf
ombur <omburlio@gmail.com>
2023-02-09 16:06:58 -0800

    Added card fields, reserving

--- main.py
+++ main.py
@@ -1,6 +1,88 @@
 import numpy as np
+import random
 
-red, blue, white, green, black, yellow = 0, 1, 2, 3, 4, 5
+black, blue, green, red, white, yellow = 0, 1, 2, 3, 4, 5
+
+colors = {"black": 0, "blue": 1, "green": 2, "red": 3, "white": 4, "yellow": 5}
+words = {v: k for k, v in colors.items()}
+
+
+def cost_string(costs):
+    cost_s = ""
+    cols = ["k", "b", "g", "r", "w"]
+    for i in range(len(costs)):
+        if costs[i] != 0:
+            cost_s += str(costs[i]) + cols[i] + " "
+    return cost_s
+
+
+def import_cards():
+    all_cards = {}
+    rank1_deck, rank2_deck, rank3_deck = {}, {}, {}
+    cards = open("splendor_cards.txt", "r").read()
+    card_list = cards.split("\n")
+    for card in card_list:
+        props = card.split(",")
+        if props[0].isnumeric():
+            all_cards[int(props[0])] = Card(int(props[0]), int(props[1]), props[3], colors[props[2]],
+                                            list(map(int, props[4:])))
+            if int(props[1]) == 1:
+                rank1_deck[int(props[0])] = Card(int(props[0]), int(props[1]), props[3], colors[props[2]],
+                                                 list(map(int, props[4:])))
+            elif int(props[1]) == 2:
+                rank2_deck[int(props[0])] = Card(int(props[0]), int(props[1]), props[3], colors[props[2]],
+                                                 list(map(int, props[4:])))
+            elif int(props[1]) == 3:
+                rank3_deck[int(props[0])] = Card(int(props[0]), int(props[1]), props[3], colors[props[2]],
+                                                 list(map(int, props[4:])))
+
+    return all_cards, [rank1_deck, rank2_deck, rank3_deck]
+
+
+def import_nobles():
+    all_nobles = {}
+    nobles = open("splendor_nobles.txt", "r").read()
+    noble_list = nobles.split("\n")
+    for noble in noble_list:
+        props = noble.split(",")
+        if props[0].isnumeric():
+            all_nobles[int(props[0])] = Noble(int(props[0]), list(map(int, props[1:])))
+    return all_nobles
+
+
+def draw_cards(deck, count):
+    chosen = []
+    for i in range(count):
+        item_id, item = random.choice(list(deck.items()))
+        del deck[item_id]
+        chosen.append(item)
+    return chosen
+
+
+class Field(object):
+    def __init__(self, deck_list):
+        self.deck_list = deck_list
+
+    def __repr__(self):
+        return str(self.deck_list)
+
+    def __len__(self):
+        return len(self.deck_list)
+
+    def id_in(self, card_id):
+        for card in self.deck_list:
+            if isinstance(card, Card):
+                if card.get_id() == card_id:
+                    return True
+        return False
+
+    def remove_id(self, card_id):
+        for card in self.deck_list:
+            if card.get_id() == card_id():
+                self.deck_list.remove(card)
+
+    def add_card(self, card):
+        self.deck_list.append(card)
 
 
 class Player(object):
@@ -9,6 +91,7 @@ class Player(object):
         self.points = 0
         self.cards = np.zeros(5)
         self.nobles = []
+        self.reserves = Field([])
 
     def get_points(self):
         return self.points
@@ -19,6 +102,9 @@ class Player(object):
     def get_gems(self):
         return self.gems
 
+    def get_reserves(self):
+        return self.reserves
+
     def set_gems(self, gem_list):
         self.gems = gem_list
 
@@ -27,7 +113,6 @@ class Player(object):
         total = 0
         for i in range(len(card_costs)):
             total += max(0, card_costs[i] - self.gems[i] + self.cards[i])
-            print(total)
         return total <= self.gems[yellow]
 
     def default_buy_card(self, card):
@@ -61,22 +146,34 @@ class Player(object):
             return False
         new_gems = np.zeros(6)
         for gem in gem_list:
+            if gem < 0 or gem > 4 or not isinstance(gem, int):
+                return False
             new_gems[int(gem)] += 1
         for j in range(len(new_gems)):
             if new_gems[j] > 2 or (new_gems[j] == 2 and game_gems[j] <= 4) or (game_gems[j] < new_gems[j]):
                 return False
         return True
 
+    def can_reserve_card(self):
+        return len(self.reserves) < 3
+
+    def reserve_card(self, card):
+        self.reserves.add_card(card)
+        return card
+
+
+
 class Card(object):
-    def __init__(self, id, rank, points, color, costs):
-        self.id = id
+    def __init__(self, card_id, rank, points, color, costs):
+        self.id = card_id
         self.points = points
         self.color = color
         self.costs = costs
         self.rank = rank
 
-    def __print__(self):
-        print(self.id)
+    def __repr__(self):
+        return "ID: "+ str(self.id) + ", " + str(self.points) + "Pt, Rank " + str(self.rank) + ", Col:" + words[
+            self.color] + ", Costs:" + cost_string(self.costs)
 
     def get_points(self):
         return self.points
@@ -93,34 +190,45 @@ class Card(object):
     def get_id(self):
         return self.id
 
+
 class Noble(object):
-    def __init__(self, costs):
+    def __init__(self, noble_id, costs):
+        self.id = noble_id
         self.costs = costs
+
+    def __repr__(self):
+        return "Noble with id " + str(self.id) + " and costs: " + cost_string(self.costs)
 
     def get_costs(self):
         return self.costs
 
+    def get_id(self):
+        return self.id
+
 
 class Game(object):
-    def __init__(self):
-        self.player_list = [Player(), Player()]
-        self.gems = np.array([6, 6, 6, 6, 6, 6])
-        self.visible_cards = np.zeros([3, 4])
-        self.card_dict = {1: Card(0, 1, 2, 1, [0, 0, 1, 2, 0])}
-        self.visible_cards[0, 0] = 1
+    def __init__(self, num_players=2):
+        self.player_list = []
+        for num in range(num_players):
+            self.player_list.append(Player())
 
+        self.gems = np.array([6, 6, 6, 6, 6, 6])
+        self.nobles = draw_cards(import_nobles(), num_players+1)
+        self.all_cards, self.decks = import_cards()
+        self.visible_cards = [Field(draw_cards(self.decks[0], 4)), Field(draw_cards(self.decks[1], 4)),
+                              Field(draw_cards(self.decks[2], 4))]
 
     def play_game(self):
         while not self.game_over():
             for i in range(len(self.player_list)):
+                player = self.player_list[i]
                 action_taken = False
                 while not action_taken:
-                    action = input(f"Player {i+1}: (Take) Gems or (Buy) Card")
+                    action = input(f"Player {i + 1}: (Take) Gems or (Buy) Card")
                     if action.strip() == "Take":
                         gems = input("What gems would you like to take?").split(",")
-                        if self.player_list[i].valid_take_gems(gems, self.gems):
-                            gem_change = self.player_list[i].take_gems(gems)
-                            print("Gem Change", gem_change)
+                        if player.valid_take_gems(gems, self.gems):
+                            gem_change = player.take_gems(gems)
                             for k in range(len(gem_change)):
                                 self.gems[k] -= gem_change[k]
                             action_taken = True
@@ -128,10 +236,35 @@ class Game(object):
                             print("Not a valid gem selection")
                         print("Available Gems:", self.gems)
                     elif action.strip() == "Buy":
-                        print(self.visible_cards)
-                        test_card = self.card_dict[self.visible_cards[0][0]]
-                        if self.player_list[i].can_buy_card(test_card):
-                            print("Can Buy Card")
+                        for field in self.visible_cards:
+                            print(field)
+                        print(player.get_reserves())
+                        card_to_buy = self.all_cards[int(input("Card ID"))]
+                        rank_index = card_to_buy.get_rank() - 1
+                        card_id = card_to_buy.get_id()
+                        if player.can_buy_card(card_to_buy):
+                            if self.visible_cards[rank_index].id_in(card_id):
+                                self.visible_cards[rank_index].remove_id(card_id)
+                                self.visible_cards[rank_index].add_card(draw_cards(self.decks[rank_index], 1))
+                                player.default_buy_card(card_to_buy)
+                                action_taken = True
+                            elif player.get_reserves().id_in(card_id):
+                                player.get_reserves().remove_id(card_id)
+                                player.default_buy_card(card_to_buy)
+                                action_taken = True
+                    elif action.strip() == "Reserve":
+                        if player.can_reserve_card():
+                            print(self.visible_cards)
+                            card_to_reserve = self.all_cards[int(input("Card ID"))]
+                            player.reserve_card(card_to_reserve)
+                            new_draw = draw_cards(self.decks[card_to_reserve.get_rank() - 1], 1)
+                            self.visible_cards[card_to_reserve.get_rank() - 1].append(new_draw[0])
+                            if player.valid_take_gems([yellow], self.gems):
+                                gem_change = player.take_gems([yellow])
+                                for k in range(len(gem_change)):
+                                    self.gems[k] -= gem_change[k]
+                        else:
+                            print("Too many cards have been reserved")
 
     def game_over(self):
         for player in self.player_list:
@@ -139,25 +272,3 @@ class Game(object):
                 return True
         return False
 
-
-def test_player_take_gems():
-    new_player = Player()
-    print(new_player.get_gems())
-    new_player.take_gems([0, 2, 3])
-    print(new_player.get_gems())
-
-
-def test_game_take_gems():
-    new_game = Game()
-    new_game.play_game()
-
-
-def test_card_purchase():
-    test_card = Card(0, 1, 2, 1, [0, 0, 1, 2, 0])
-    new_player = Player()
-    new_player.set_gems([0, 1, 2, 2, 2, 0])
-    x = new_player.can_buy_card(test_card)
-    print(x)
-
-# test_game_take_gems()
-test_card_purchase()
---
+++ splendor_tests.py
@@ -0,0 +1,39 @@
+from main import *
+
+
+def test_player_take_gems():
+    new_player = Player()
+    print(new_player.get_gems())
+    new_player.take_gems([0, 2, 3])
+    print(new_player.get_gems())
+
+
+def play_game():
+    new_game = Game()
+    new_game.play_game()
+
+
+def test_card_purchase():
+    test_card = Card(0, 1, 2, 1, [0, 0, 1, 2, 0])
+    new_player = Player()
+    new_player.set_gems([0, 1, 2, 2, 2, 0])
+    x = new_player.can_buy_card(test_card)
+    print(x)
+
+
+def test_import_cards():
+    all_cards, decks = import_cards()
+    for card in all_cards:
+        print(all_cards[card])
+
+
+def test_import_nobles():
+    all_nobles = import_nobles()
+    noble_list = draw_cards(all_nobles, 3)
+    print(noble_list)
+    for noble in noble_list:
+        print(noble)
+
+
+play_game()
+
